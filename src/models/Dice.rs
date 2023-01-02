1a0e207b4a1b940b89c08e3b283f9ec1076ff662
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-01-01 21:44:26 -0600

    first commit

---
+++ src/models/Dice.rs
@@ -0,0 +1,17 @@
+use rand::Rng; 
+
+// 12/31 7:35 
+#[derive(Debug)]
+pub struct Dice {
+    minValue: i32,
+    maxValue: i32,
+    totalRollValue: i32,
+}
+
+impl Dice {
+    fn roll() {
+        let mut rng = rand::thread_rng(); 
+        return rng.gen_range(minValue..maxValue + 1); 
+    }
+}
+
